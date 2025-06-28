6091bb8799baef43be5ae932c599aabd4200136c
Mathias <mathiasdejesus86@gmail.com>
2025-06-28 13:35:31 -0300

    condicionales

---
+++ Clase 1/clase 2/condicionales.py
@@ -0,0 +1,26 @@
+'''
+Condicionales if
+
+'''
+dato=int(input(''Ingrese unn número: ''))
+if dato > 0:
+    print(''El número es positivo'')    
+    elif dato < 0:
+    print(''El número es negativo'')
+else:
+    print(''El número es cero'')
+
+#un if
+if dato > 0:
+    print('El número es positivo') 
+#un if y un else
+if dato > 0:
+    print('El número es positivo') 
+#if elif y else
+if dato > 0:
+    print('El número es positivo')    
+elif dato < 0:
+    print('El número es negativo')
+else:
+    print('El número es cero')
+  
