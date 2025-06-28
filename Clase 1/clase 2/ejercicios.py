94d4728492b64add7faab32885c189d4963f468e
Mathias <mathiasdejesus86@gmail.com>
2025-06-28 14:15:37 -0300

    ejercicio

--- Clase 1/clase 2/ejercicios.py
+++ Clase 1/clase 2/ejercicios.py
@@ -8,5 +8,13 @@ o si ambos lo son.
 n1=int(input("Ingrese un número: "))
 n2=int(input("Ingrese otro número: "))
 
-if n1 % 2 == 0 and n2 % 2 == 0:
+if ((n1%2 ==0)) and ((n2%2 == 0)):
 	print("Ambos son pares")
+elif (n1%2 == 0) and (n2%2 != 0):
+    print(f"El primer número {n1} es par")
+    print(f"El segundo número {n2} es impar")
+elif (n1%2 != 0) and (n2%2 == 0):
+    print(f"El primer número {n1} es impar")
+    print(f"El segundo número {n2} es par")
+else:
+     print("Los dos numeros son impares")
