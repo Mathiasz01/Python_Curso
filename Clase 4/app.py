6fa1904470c305e7ec2dd7c90b67f49ed1ebdba5
Mathias <mathiasdejesus86@gmail.com>
2025-07-17 17:38:42 -0300

    appy

--- Clase 4/app.py
+++ Clase 4/app.py
@@ -15,4 +15,7 @@ def unida():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host=('0.0.0.0'))
-            
+
+'''
+0.0.0.0 es una dirección IP que permite que la aplicación Flask sea accesible desde cualquier dirección IP de la red local.
+'''
