7da27a510b2d6b5364e24b25fdb31e916419e393
Mathias <mathiasdejesus86@gmail.com>
2025-07-17 19:11:19 -0300

    aritméticos

---
+++ clase 2/aritmeticos.py
@@ -0,0 +1,14 @@
+'''
+Ejemplo de funciones aritméticas
+'''
+num1 = 2
+num2 = 1
+print('Resultado de la suma:', num1 + num2)
+print('Resultado de la resta:', num1 - num2)
+
+''' Multiplicación y división'''
+
+num1 = 10
+num2 = 4
+print('Resultado de la multiplicación:', num1 * num2)
+print('Resultado de la división:', num1 / num2)
